79289e152d836272fedaf5f5e929712a722ff35a
Malik Dimter <malik.dimter@gmail.com>
2019-01-15 17:16:19 +0100

    css background image

---
+++ precache-manifest.1b4164b12664059e1ea05c1af6dcf94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "467616f2a4f116ead59a",
+    "url": "/portfolio/static/js/main.467616f2.chunk.js"
+  },
+  {
+    "revision": "fbc86a675c04d3fd5df4",
+    "url": "/portfolio/static/js/1.fbc86a67.chunk.js"
+  },
+  {
+    "revision": "467616f2a4f116ead59a",
+    "url": "/portfolio/static/css/main.ad148c21.chunk.css"
+  },
+  {
+    "revision": "fbc86a675c04d3fd5df4",
+    "url": "/portfolio/static/css/1.5db9ddba.chunk.css"
+  },
+  {
+    "revision": "fcdc24950a12efdea9c1cb525842dee7",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.30c1533678fb13196a85b739694241f7.js"
+  "/portfolio/precache-manifest.1b4164b12664059e1ea05c1af6dcf94d.js"
 );
 
 workbox.clientsClaim();
